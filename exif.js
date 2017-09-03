f14d786399c23a051b37a192d6a0a8eb9a940618
Tom Lorimer <tom@purplehippo.io>
2017-09-03 10:30:47 +0100

    tried a different no-cache way

--- exif.js
+++ exif.js
@@ -401,8 +401,9 @@
                     }
                     http = null;
                 };
-                http.open("GET", img.src+"?cb="+new Date().getTime(), true);
+                http.open("GET", img.src, true);
                 http.responseType = "arraybuffer";
+                http.setRequestHeader("Cache-Control", "no-cache");
                 http.send(null);
             }
         } else if (self.FileReader && (img instanceof self.Blob || img instanceof self.File)) {
